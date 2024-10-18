d1acd5fdb2bfb0d49bcd9ae353c42c2c517ed451
tomfridental1 <tom.fridental@bigpanda.io>
2024-10-18 14:24:40 +0300

    Add first utils

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
-export const foo = () => {
-  return 2;
+import { screen, MatcherOptions } from "@testing-library/react";
+
+export const getText = (text: string, options?: MatcherOptions) => {
+  return screen.getByText('text', options);
+}
+
+export const getAsyncText = async (text: string, options?: MatcherOptions) => {
+  return await screen.findByText('text', options);
 }
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,5 @@
-// import commonjs from '@rollup/plugin-commonjs';
-// import resolve from '@rollup/plugin-node-resolve';
-// import del from 'rollup-plugin-delete';
-// import typescript from '@rollup/plugin-typescript';
-// import svgr from '@svgr/rollup';
-// import terser from '@rollup/plugin-terser';
-// import image from '@rollup/plugin-image';
-// import copy from 'rollup-plugin-copy'
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
 export default {
@@ -17,19 +11,9 @@ export default {
       interop: 'compat',
     },
   ],
-  // external: ['react', 'react-dom', 'styled-components'],
-  // plugins: [
-  //   del({ targets: 'library/*' }),
-  //   image({ exclude: '/**/*.svg' }),
-  //   svgr(),
-  //   resolve({ browser: true }),
-  //   commonjs(),
-  //   typescript({ exclude: ['**/*.test.tsx'] }),
-  //   terser({ compress: { evaluate: false, join_vars: false } }),
-  //   copy({
-  //     targets: [
-  //       { src: 'components/composite/Table-v2/table-style.css', dest: 'library' },
-  //     ]
-  //   })
-  // ],
+  plugins: [
+    commonjs(),
+    typescript(),
+  ],
+  external: ['@testing-library/dom', '@testing-library/react'],
 };
